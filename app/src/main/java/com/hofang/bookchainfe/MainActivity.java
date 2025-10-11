7eea1e0a7dc1889542f7c44a437155b9b487a691
hofang42 <ringhost42@gmail.com>
2025-10-11 15:59:58 +0700

    UI for address, profile

--- app/src/main/java/com/hofang/bookchainfe/MainActivity.java
+++ app/src/main/java/com/hofang/bookchainfe/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+
+import com.hofang.bookchainfe.ui.address.AddAddressBottomSheet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Button openBtn = findViewById(R.id.btn_add_delivery);
+        if (openBtn != null) {
+            openBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AddAddressBottomSheet sheet = new AddAddressBottomSheet();
+                    sheet.show(getSupportFragmentManager(), "add_address_sheet");
+                }
+            });
+        }
+        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        //     return insets;
+        // });
     }
 }
