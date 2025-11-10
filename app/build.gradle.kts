b23550155e7d1a6f4ce52367713999713264fb75
Nomsociuu <nomsociuu2004@gmail.com>
2025-11-10 22:52:23 +0700

    add missing build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,29 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    // --- BẠN CẦN THÊM KHỐI NÀY ---
+    // Thêm khối packaging để giải quyết xung đột file META-INF
+    packaging {
+        resources {
+            // Loại bỏ file INDEX.LIST gây ra lỗi
+            excludes.add("META-INF/INDEX.LIST")
+
+            // (Nên thêm) Loại bỏ các file metadata khác
+            // thường gây xung đột
+            excludes.add("META-INF/DEPENDENCIES")
+            excludes.add("META-INF/LICENSE")
+            excludes.add("META-INF/LICENSE.txt")
+            excludes.add("META-INF/NOTICE")
+            excludes.add("META-INF/NOTICE.txt")
+            excludes.add("META-INF/io.netty.versions.properties")
+        }
+    }
+    // --- KẾT THÚC KHỐI THÊM VÀO ---
 }
 
 dependencies {
+    // ... (toàn bộ phần dependencies của bạn giữ nguyên)
 
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -48,17 +68,23 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    
+
     // JSON parsing
     implementation("com.google.code.gson:gson:2.10.1")
-    
+
     // SharedPreferences (for token storage)
     implementation("androidx.preference:preference:1.2.1")
-    
+
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+    implementation("com.google.firebase:firebase-storage")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.firebase.appdistribution.gradle)
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
